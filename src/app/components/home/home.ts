c455fda7c8810215f775042ef132c3f62cd94584
JHON-MCNEIL <23-42069@g.batstate-u.edu.ph>
2025-10-26 00:24:18 +0800

    deployment

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { HomeComponent } from './home.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HomeComponent, RouterLink],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
